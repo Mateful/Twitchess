bb92f8874a7e3b9f70cd07cf4d949a246aa1ea96
SancezZ <darkspicy@gmx.de>
2011-12-03 12:59:13 +0100

    GenerateImage kleine Stylefixes
    Bilden (Schachbrett + Spielfiguren)hinzugefügt

--- src/de/fhb/projects/chesstwitterbot/image/GenerateImage.java
+++ src/de/fhb/projects/chesstwitterbot/image/GenerateImage.java
@@ -11,10 +11,6 @@ import javax.swing.JLabel;
 
 public class GenerateImage extends JFrame {
 	
-	
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public GenerateImage() {
@@ -26,9 +22,6 @@ public class GenerateImage extends JFrame {
 		}
     }
  
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         new GenerateImage();
     }
@@ -36,7 +29,8 @@ public class GenerateImage extends JFrame {
     public void interpretFan(String fan) throws IOException{
     	int row=0,column=0;
     	fan="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    	BufferedImage backgroundImage = resetImage();
+    		BufferedImage backgroundImage = resetImage();
+    	
     	for(int i=0; ' '!=fan.charAt(i);i++){
     		if('r'==fan.charAt(i)){
     			generateImage("rook","-b",row, column,backgroundImage);
@@ -66,7 +60,6 @@ public class GenerateImage extends JFrame {
             		}        			
         		}    			
     		}    	
-    		System.out.println("row="+row+"\n column="+column+"\nfanchar="+fan.charAt(i));
     		column = (column+1 + charToInt(fan.charAt(i))) % 8;
     	}   
     	createFinalImage(backgroundImage);
@@ -86,40 +79,17 @@ public class GenerateImage extends JFrame {
     	
     	BufferedImage backgroundImage =    	
 			backgroundImage = ImageIO.read(new File(
-			        "field.png"));
-	
-    	
+			        "img/field.png"));    	
     	return backgroundImage;
     }
-    
-    public int charToInt(char ch){
- 
-    	if(ch=='1')return 0;
-    	if(ch=='2')return 1;
-    	if(ch=='3')return 2;
-    	if(ch=='4')return 3;
-    	if(ch=='5')return 4;
-    	if(ch=='6')return 5;
-    	if(ch=='7')return 6;
-    	if(ch=='8')return 7;
-    	return 0;
-    }
-    
     
     public void generateImage(String figure, String colour, int row, int column, BufferedImage backgroundImage){
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
     	try {
-            
-            
             BufferedImage foregroundImage = ImageIO
-                    .read(new File(figure+colour+".png"));            
-       
-            System.out.println("rowC="+rowToCoordinate(row)+"\n columnC="+ columnToCoordinate(column));
+                    .read(new File("img/"+figure+colour+".png")); 
             backgroundImage.getGraphics()
                     .drawImage(foregroundImage,columnToCoordinate(column), rowToCoordinate(row), this);
-           
-            
- 
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -131,22 +101,7 @@ public class GenerateImage extends JFrame {
         add(label);
         pack();
         setVisible(true);
-    }
- 
-    public int rowToCoordinate(int row){
-    	switch(row){
-    	case 0: return -2;
-    	case 1: return 52;
-    	case 2: return 108;
-    	case 3: return 162;
-    	case 4: return 218;
-    	case 5: return 274;
-    	case 6: return 330;
-    	case 7: return 386;
-    	default: return 0;
-    	}
-    }
-    
+    }    
     
     public int columnToCoordinate(int column){
     	switch(column){
@@ -161,6 +116,33 @@ public class GenerateImage extends JFrame {
     	default: return 0;
     	}
     } 
+    
+    public int rowToCoordinate(int row){
+    	switch(row){
+    	case 0: return -2;
+    	case 1: return 52;
+    	case 2: return 108;
+    	case 3: return 162;
+    	case 4: return 218;
+    	case 5: return 274;
+    	case 6: return 330;
+    	case 7: return 386;
+    	default: return 0;
+    	}
+    }
+    
+    public int charToInt(char ch){
+    	 
+    	if(ch=='1')return 0;
+    	if(ch=='2')return 1;
+    	if(ch=='3')return 2;
+    	if(ch=='4')return 3;
+    	if(ch=='5')return 4;
+    	if(ch=='6')return 5;
+    	if(ch=='7')return 6;
+    	if(ch=='8')return 7;
+    	return 0;
+    }
 }
 
 
